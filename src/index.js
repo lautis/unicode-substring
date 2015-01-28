37ed159c8f76fbd94821eb2c0d4c16e35a88cbb8
Ville Lautanala <lautis@gmail.com>
2015-01-28 22:24:49 +0200

    Implement unicode-aware substring

---
+++ src/index.js
@@ -0,0 +1,49 @@
+function charAt(string, index) {
+  var first = string.charCodeAt(index);
+  var second;
+  if (first >= 0xD800 && first <= 0xDBFF && string.length > index + 1) {
+    second = string.charCodeAt(index + 1);
+    if (second >= 0xDC00 && second <= 0xDFFF) {
+      return string.substring(index, index + 2);
+    }
+  }
+  return string[index];
+}
+
+function slice(string, start, end) {
+  var accumulator = "";
+  var character;
+  var stringIndex = 0;
+  var unicodeIndex = 0;
+  var length = string.length;
+
+  while (stringIndex < length) {
+    character = charAt(string, stringIndex);
+    if (unicodeIndex >= start && unicodeIndex < end) {
+      accumulator += character;
+    }
+    stringIndex += character.length;
+    unicodeIndex += 1;
+  }
+  return accumulator;
+}
+
+function toNumber(value, fallback) {
+  if (value === undefined) {
+    return fallback;
+  } else {
+    return Number(value);
+  }
+}
+
+export default function (string, start, end) {
+  var realStart = toNumber(start, 0);
+  var realEnd = toNumber(end, string.length);
+  if (realEnd == realStart) {
+    return "";
+  } else if (realEnd > realStart) {
+    return slice(string, realStart, realEnd);
+  } else {
+    return slice(string, realEnd, realStart);
+  }
+}
---
+++ test/substring.test.js
@@ -0,0 +1,42 @@
+import assert from "assert";
+import substring from "../src/index";
+
+describe("unicode-substring", () => {
+  var string = "Photo booth keytar Williamsburg twee selfies.";
+  var unicodeString = "💥💣 emoji are awesome 💣💣💣 yeah";
+
+  it("returns the full string if no start and end parameters are given", () => {
+    assert.equal(substring(string), string);
+  });
+
+  it("skips characters before start position", () => {
+    assert.equal(substring(string, 10), string.substring(10));
+  });
+
+  it("omits characters after end position", () => {
+    assert.equal(substring(string, 0, 10), string.substring(0, 10));
+  });
+
+  it("swaps arguments if start > end", () => {
+    assert.equal(substring(string, 10, 0), string.substring(0, 10));
+  });
+
+  it("doesn't break on negative indices", () => {
+    assert.equal(substring(string, -1, 10), string.substring(0, 10));
+    assert.equal(substring(string, 0, -1), string.substring(0, 0));
+  });
+
+  it("returns empty string if start and end are the same", () => {
+    assert.equal(substring(string, 11, 11), "");
+  });
+
+  it("returns the rest of string when end is omitted", () => {
+    assert.equal(substring(string, 1), string.substring(1));
+  });
+
+  it("consider surrogate pairs as one character", () => {
+    assert.equal(substring(unicodeString, 0, unicodeString.length - 5), unicodeString);
+    assert.equal(substring(unicodeString, 3, 8), "emoji");
+    assert.equal(substring(unicodeString, 9, 22), "are awesome 💣");
+  });
+});
